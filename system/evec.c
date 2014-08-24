6e4c65aca964dea6a9ee3b8182b82a9d254f900b
Rajas H Karandikar <rkarandi@purdue.edu>
2014-08-24 13:05:58 -0700

    IRQ handling function names changed to irq_disp (asm function) and irq_dispatch (C function)

--- system/evec.c
+++ system/evec.c
@@ -66,10 +66,10 @@ int32	set_evec(uint32 xnum, uint32 handler)
 }
 
 /*-------------------------------------------------------------------------
- * irq_handler - call the handler for specific interrupt
+ * irq_dispatch - call the handler for specific interrupt
  *-------------------------------------------------------------------------
  */
-void	irq_handler()
+void	irq_dispatch()
 {
 	struct	intc_csreg *csrptr = (struct intc_csreg *)0x48200000;
 	uint32	xnum;		/* Interrupt number of device	*/
